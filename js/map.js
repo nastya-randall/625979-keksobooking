81f37e1db7ec6448f19c7c2b4eed343fbc3e1324
Unknown <anastassiya.sviridova@gmail.com>
2018-06-10 18:45:54 +0200

    Draft

--- js/map.js
+++ js/map.js
@@ -47,19 +47,23 @@ var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
 var MIN_ROOMS = 1;
 var MAX_ROOMS = 5;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 var ads = [];
 var ADS_NUMBER = 8;
-var authors = [];
 var locations = [];
-var offers = [];
 
 var map = document.querySelector('.map');
 
 var pins = document.querySelector('.map__pins');
+var filters = document.querySelector('.map__filters-container');
 var pinTemplate = document.querySelector('template')
     .content
     .querySelector('.map__pin');
 
+var cardTemplate = document.querySelector('template')
+    .content
+    .querySelector('.map__card');
 
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -73,7 +77,9 @@ var getRandom = function (array) {
   return Math.floor(Math.random() * array.length);
 };
 
-var shuffleArray = function (array) { // Fisher-Yates Shuffle
+// Fisher-Yates Shuffle
+
+var shuffleArray = function (array) {
   var currentIndex = array.length;
   var temporaryValue;
   var randomIndex;
@@ -96,20 +102,7 @@ function getRandomShuffledArray(arr) {
   return arr.slice(counter, arr.length);
 }
 
-var renderAuthors = function () {
-  for (var i = 0; i < ADS_NUMBER; i++) {
-    var avatarRand = getRandom(avatars);
-    var imgAddress = 'img/avatars/user' + avatars[avatarRand] + '.png';
-    var author = {
-      avatar: imgAddress
-    };
-    avatars.splice(avatarRand, 1);
-    authors.push(author);
-  }
-};
-
-renderAuthors();
-console.log(authors);
+// получение координат меток
 
 var renderLocation = function () {
   for (var i = 0; i < ADS_NUMBER; i++) {
@@ -124,12 +117,15 @@ var renderLocation = function () {
 
 renderLocation();
 
+// герерация объявлений
+
 var renderAds = function () {
   for (var i = 0; i < ADS_NUMBER; i++) {
+    var avatarRand = getRandom(avatars);
     var titleRand = getRandom(titles);
     var ad = {
       author: {
-        avatar: authors[i].avatar
+        avatar: 'img/avatars/user' + avatars[avatarRand] + '.png'
       },
       offer: {
         title: titles[titleRand],
@@ -137,7 +133,7 @@ var renderAds = function () {
         price: getRandomInRange(MIN_PRICE, MAX_PRICE),
         type: getRandomArrayValue(types),
         rooms: getRandomInRange(MIN_ROOMS, MAX_ROOMS),
-        guests: getRandomInRange(MIN_ROOMS, MAX_ROOMS),
+        guests: getRandomInRange(2, MAX_ROOMS),
         checkin: getRandomArrayValue(checkinTime),
         checkout: getRandomArrayValue(checkinTime),
         features: getRandomShuffledArray(features),
@@ -149,6 +145,7 @@ var renderAds = function () {
         y: locations[i].y
       }
     };
+    avatars.splice(avatarRand, 1);
     titles.splice(titleRand, 1);
     ads.push(ad);
   }
@@ -157,18 +154,94 @@ var renderAds = function () {
 renderAds();
 console.log(ads);
 
+// определение типа жилья
+
+var getType = function (item) {
+  var typeText;
+  if (item.offer.type === 'flat') {
+    return 'Квартира';
+  } else if (item.offer.type === 'bungalo') {
+    return 'Бунгало';
+  } else if (item.offer.type === 'house') {
+    return 'Дом';
+  } else if (item.offer.type === 'palace') {
+    return 'Дворец';
+  }
+  return typeText;
+};
+
+// создание метки
+
 var renderPin = function (item) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = item.location.x + 'px';
-  pinElement.style.top = item.location.y + 'px';
-  pinElement.src = authors.avatar;
+  pinElement.style.left = item.location.x - Math.floor(PIN_WIDTH / 2) + 'px';
+  pinElement.style.top = item.location.y - PIN_HEIGHT + 'px';
+  pinElement.querySelector('img').src = item.author.avatar;
+  pinElement.querySelector('img').alt = item.offer.title;
   return pinElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < ADS_NUMBER; i++) {
-  fragment.appendChild(renderPin(ads[i]));
-}
+// вставка меток на страницу
 
-pins.appendChild(fragment);
+var addPins = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < ADS_NUMBER; i++) {
+    fragment.appendChild(renderPin(ads[i]));
+  }
+  pins.appendChild(fragment);
+};
+
+addPins();
+
+// жалкие мои попытки хоть что.то сделать с features
+
+var featuresList = cardTemplate.querySelector('.popup__features');
+var renderFeatures = function (item) {
+  for (var i = 0; i < featuresList.children.length; i++) {
+    if (!featuresList.children[i].classList.contains(item.features)) {
+      featuresList.removeChild(featuresList.childNodes[i]);
+    }
+  }
+  return featuresList.children;
+};
+
+renderFeatures(ads[0]);
+console.log(featuresList.children);
+
+// и такие же жалкие заставить photos работать, но ниче не работает
+
+var photosContainer = cardTemplate.querySelector('.popup__photos');
+
+var renderPhotos = function (item) {
+  var photoElement = cardTemplate.querySelector('.popup__photo').cloneNode;
+  photoElement.src = item.offer.photos;
+  return photoElement;
+};
+
+// герерация карточки объявления
+
+var renderCard = function (item) {
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = item.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = item.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = item.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = getType(item);
+  cardElement.querySelector('.popup__text--capacity').textContent = item.offer.rooms + ' комнаты для ' + item.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после  ' + item.offer.checkin + ', выезд до ' + item.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = item.offer.description;
+  renderFeatures(item);
+
+  return cardElement;
+};
+
+// вставка карточки на страницу
+
+var addCards = function () {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(renderCard(ads[0]));
+  map.insertBefore(fragment, filters);
+};
+
+addCards();
+
 map.classList.remove('map--faded');
